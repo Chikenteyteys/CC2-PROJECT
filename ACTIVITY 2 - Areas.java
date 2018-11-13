a3ee55a4429ef53b3759554b78c70017aa2c6fc6
pauljmvalter <44995678+pauljmvalter@users.noreply.github.com>
2018-11-13 16:03:57 +0800

    Update and rename Areas.java to ACTIVITY 2 - Areas.java

--- ACTIVITY 2 - Areas.java
+++ ACTIVITY 2 - Areas.java
@@ -6,28 +6,28 @@ public class Areas_Eligio {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Radius of the Circle -> ");
+        System.out.print("ENTER THE RADIUS OF THE CIRCLE: ");
         
         double radius = sc.nextDouble();
         double AreaCircle = Math.PI * (radius * radius);
-        System.out.println("The area of circle is: " + AreaCircle);
+        System.out.println("THE AREA OF THE CIRCLE IS: " + AreaCircle);
         double circumference= Math.PI * 2*radius;
-        System.out.println("The circumference of the circle is: " + circumference + "\n\n\n");
+        System.out.println("THE CIRCUMFERENCE OF THE CIRCLE IS: " + circumference + "\n\n\n");
         
         
         double size, AreaSquare;
-        System.out.print("Enter the size of square -> ");
+        System.out.print("ENTER THE ARE OF THE SQUARE: ");
         size = sc.nextDouble();
         AreaSquare = Math.pow(size, 2);
-        System.out.println("Area of a Squre: " + AreaSquare + "\n\n\n");
+        System.out.println("THE AREA OF THE SQUARE IS: " + AreaSquare + "\n\n\n");
         
         double height, base, AreaTriangle;
-        System.out.print("Enter the height of the triangle -> ");
+        System.out.print("ENTER HEIGHT OF THE TRIANGLE: ");
         height = sc.nextInt();
-        System.out.print("Enter the base of the triangle -> ");
+        System.out.print("ENTER THE BASE OF THE TRIANGLE: ");
         base = sc.nextInt();
         AreaTriangle = ((height * base) / 2);
-        System.out.println("Area of the triangle is : " + AreaTriangle);
+        System.out.println("THE AREA OF THE TRIANGLE IS: " + AreaTriangle);
         
     }
     
